12d24260c7bcda3361c070f0fca13ca0cb472ff5
pogx402 <240068810+pogx402@users.noreply.github.com>
2025-10-26 12:20:32 -0400

    fix: Remove EIP712Domain from types object to fix ambiguous primary types error

--- script.js
+++ script.js
@@ -341,12 +341,6 @@ async function handleProtocolClick() {
 	                };
 	
 	                const types = {
-	                    EIP712Domain: [
-	                        { name: 'name', type: 'string' },
-	                        { name: 'version', type: 'string' },
-	                        { name: 'chainId', type: 'uint256' },
-	                        { name: 'verifyingContract', type: 'address' },
-	                    ],
 	                    TransferWithAuthorization: [
 	                        { name: 'from', type: 'address' },
 	                        { name: 'to', type: 'address' },
